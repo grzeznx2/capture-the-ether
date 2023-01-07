e23442f2ac418f22e769fce474337f6e0a529126
grzeznx2 <grzeznx2@gmail.com>
2023-01-07 12:51:24 +0100

    Solve GuessTheNewNumber challenge

---
+++ test/lotteries/GuessTheNewNumber.spec.ts
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+const { utils } = ethers;
+
+describe("GuessTheNewNumberChallenge", () => {
+  it.only("Solves the challenge", async () => {
+    const value = utils.parseEther("1")
+    const [deployer,attacker] = await ethers.getSigners()
+    const contractFactory = await ethers.getContractFactory("GuessTheNewNumberChallenge")
+    const contract = await contractFactory.connect(deployer).deploy({value})
+    await contract.deployed()
+
+    const attackerContractFactory = await ethers.getContractFactory("GuessTheNewNumberChallengeAttacker")
+    const attackerContract = await attackerContractFactory.connect(attacker).deploy(contract.address)
+    await attackerContract.deployed()
+
+    const attackTx = await attackerContract.attack({value})
+    await attackTx.wait;
+
+    expect(await contract.isComplete()).to.be.true;
+  });
+});
